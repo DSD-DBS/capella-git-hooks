99c589e833ccfab2b102c2767b4372de7eebe885
Michael Harbarth <michael.harbarth@deutschebahn.com>
2023-10-10 07:49:31 +0200

    feat: Add project files

---
+++ capella_git_hooks/__init__.py
@@ -0,0 +1,10 @@
+# Copyright DB Netz AG and contributors
+# SPDX-License-Identifier: Apache-2.0
+"""The capella_git_hooks package."""
+from importlib import metadata
+
+try:
+    __version__ = metadata.version("capella_git_hooks")
+except metadata.PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0+unknown"
+del metadata
---
+++ tests/test_capella_git_hooks.py
@@ -0,0 +1,8 @@
+# Copyright DB Netz AG and contributors
+# SPDX-License-Identifier: Apache-2.0
+
+import capella_git_hooks
+
+
+def test_add_some_tests_here():
+    ...
