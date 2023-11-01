fa41f2a08c1a248d66ecb167446b520bb7490d2b
ewuerger <ernst.wuerger@gmail.com>
2023-11-01 13:54:35 +0100

    fix: Fix various missing links
    - Fix semantic resources
    - Include `commit:/` prefixed references
    - Include `UID` references

--- capella_git_hooks/fix_links.py
+++ capella_git_hooks/fix_links.py
@@ -12,12 +12,23 @@ import sys
 from capellambse.loader import exs
 from lxml import etree as ET
 
+DIAGRAM_OVERVIEW_TAG = "{http://www.eclipse.org/sirius/1.1.0}DAnalysis"
+
 # pylint: disable=line-too-long
-root_pattern = re.compile(
-    r"(index:/).*\.(capella|aird)#[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}"
+prefix_pattern = r"(commit:/|index:/)"
+reference_pattern = re.compile(
+    r"(#[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}|#_[0-9a-zA-Z_-]{22})"
 )
+root_pattern = re.compile(rf"{prefix_pattern}.*\.(capella|aird)")
+root_reference_pattern = re.compile(
+    f"{root_pattern.pattern}{reference_pattern.pattern}"
+)
+
 fragment_pattern = re.compile(
-    r"(index:/fragments/).*\.(capella|aird)fragment#[0-9a-f]{8}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{4}\b-[0-9a-f]{12}"
+    rf"{prefix_pattern}fragments/.*\.(capella|aird)fragment"
+)
+fragment_reference_pattern = re.compile(
+    f"{fragment_pattern.pattern}{reference_pattern.pattern}"
 )
 file_name_pattern = re.compile(r".*\.(capella|aird)(fragment)?")
 
@@ -43,6 +54,34 @@ def get_unmodified_tracked_files() -> list[str]:
         return []
 
 
+def search_and_replace(
+    pattern: re.Pattern, value: str, replacement: str
+) -> str | None:
+    while match := pattern.search(value):
+        index_match = match.span(1)
+        value = value[: index_match[0]] + replacement + value[index_match[1] :]
+    return value
+
+
+def fix_semantic_resources(
+    root: ET._Element, root_repl: str, fragment_repl: str
+) -> bool:
+    changed = False
+    danalysis = next(root.iterchildren(DIAGRAM_OVERVIEW_TAG))
+    for resource in danalysis.iterchildren("semanticResources"):
+        if resource.text is None:
+            continue
+
+        text = search_and_replace(
+            fragment_pattern, resource.text, fragment_repl
+        )
+        text = search_and_replace(root_pattern, resource.text, root_repl)
+        if resource.text != text:
+            resource.text = text
+            changed = True
+    return changed
+
+
 def fix_xml(path: str) -> bool:
     """Fix the XML located in the provided path and write changes to disk."""
     is_fragment = path.startswith("fragments/")
@@ -60,28 +99,27 @@ def fix_xml(path: str) -> bool:
     root_replacement = "../" if is_fragment else ""
     fragment_replacement = "" if is_fragment else "fragments/"
 
+    try:
+        changed = fix_semantic_resources(
+            root, root_replacement, fragment_replacement
+        )
+    except StopIteration:
+        pass
+
     for elem in root.iter():
         for key, value in list(elem.attrib.items()):
-            # Use the regex pattern to find and replace attribute values
-            while match := root_pattern.search(value):
-                changed = True
-                index_match = match.span(1)
-                value = (
-                    value[: index_match[0]]
-                    + root_replacement
-                    + value[index_match[1] :]
-                )
+            if value is None:
+                continue
 
-            while match := fragment_pattern.search(value):
+            new_value = search_and_replace(
+                root_reference_pattern, value, root_replacement
+            )
+            new_value = search_and_replace(
+                fragment_reference_pattern, value, fragment_replacement
+            )
+            if value != new_value:
+                elem.attrib[key] = new_value
                 changed = True
-                index_match = match.span(1)
-                value = (
-                    value[: index_match[0]]
-                    + fragment_replacement
-                    + value[index_match[1] :]
-                )
-
-            elem.attrib[key] = value
 
     if changed:
         exs.write(tree, path, line_length=line_length, siblings=True)
@@ -106,8 +144,8 @@ def main():
             if fix_xml(file):
                 changed_files.append(file)
 
-    if changed_files:
-        commit_changes(changed_files)
+    # if changed_files:
+    #     commit_changes(changed_files)
 
 
 if __name__ == "__main__":
